c33fb4ac16330ae7353962e721ac333dd045ecd4
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-09-26 20:50:19 +0530

    define game winning positions

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -8,8 +8,13 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     // 0 -> X
     // 1 -> O
-    int ActivePlayer=0; // the active player will be "X" by default. // 0 -> X , 1 -> O, 2 -> NULL.
+    int ActivePlayer=0; // The active player will be "X" by default. // 0 -> X , 1 -> O, 2 -> NULL.
     int [] GameState={2,2,2,2,2,2,2,2,2}; // Starting state of the game.
+    int[][] WinPositions={ // Game winning positions on the grid.
+            {0,1,2},{3,4,5},{6,7,8},
+            {0,3,6},{1,4,7},{2,5,8},
+                {0,4,8},{2,4,6}
+    };
     public void OnTap(View view){
 
     }
