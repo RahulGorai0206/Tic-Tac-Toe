55e170ed8fc8b2b7f131620f25859d2be8d297f5
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-09-26 20:43:57 +0530

    define player and state of the game again

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     // 0 -> X
     // 1 -> O
-    int ActivePlayer=0; // the active player will be "X" by default.
+    int ActivePlayer=0; // the active player will be "X" by default. // 0 -> X , 1 -> O, 2 -> NULL.
+    int [] GameState={2,2,2,2,2,2,2,2,2}; // Starting state of the game.
     public void OnTap(View view){
 
     }
