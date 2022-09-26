0821a06ba05ea62685ab7c1bff57ef2562aa87d3
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-09-26 20:39:36 +0530

    define player properties

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 0 -> X
+    // 1 -> O
+    int ActivePlayer=0; // the active player will be "X" by default.
     public void OnTap(View view){
 
     }
